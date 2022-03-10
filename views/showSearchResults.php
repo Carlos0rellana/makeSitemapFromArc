e08b5be048fd013e5e0146bd97c46a511d1e43ff
Carlos Orellana <carlos.orellana@publimetro.cl>
2022-03-10 15:43:14 -0300

    se cambian nombres de variables de entorno

--- views/showSearchResults.php
+++ views/showSearchResults.php
@@ -1,13 +1,14 @@
 <?php
     //var_dump($argv);
-    parse_str($argv[1], $params);
-    parse_str($argv[2], $params);
-    var_dump($params);
+    parse_str($argv[1], $site);
+    parse_str($argv[2], $date);
+    var_dump($site);
+    var_dump($date);
     define('ROOT_DIR', dirname(__FILE__,2));
     require_once ROOT_DIR.'/controllers/getDataArc.php';
     if(isset($_GET["start"]) && isset($_GET["site"])){
         autoBucle($_GET["site"],$_GET["start"]);
-    }elseif(array_key_exists('start',$params) && array_key_exists('site',$params)){
-        autoBucle($params["site"],$params["start"]);
+    }elseif(array_key_exists('start',$site) && array_key_exists('site',$date)){
+        autoBucle($site["site"],$date["start"]);
     }
 ?>
