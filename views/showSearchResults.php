3f01f1507759b11b85f69151acc514e60c11edde
Carlos Orellana <carlos.orellana@publimetro.cl>
2022-03-10 15:29:48 -0300

    se corrije elseif

--- views/showSearchResults.php
+++ views/showSearchResults.php
@@ -6,7 +6,7 @@
     require_once ROOT_DIR.'/controllers/getDataArc.php';
     if(isset($_GET["start"]) && isset($_GET["site"])){
         autoBucle($_GET["site"],$_GET["start"]);
-    }else if(array_key_exists('start',$params) && array_key_exists('site',$params)){
+    }elseif(array_key_exists('start',$params) && array_key_exists('site',$params)){
         autoBucle($params["site"],$params["start"]);
     }
 ?>
