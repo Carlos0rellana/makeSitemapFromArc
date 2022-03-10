5530798598214dfd97f1c75c2e24a31a3351d429
Carlos Orellana <carlos.orellana@publimetro.cl>
2022-03-10 15:49:46 -0300

    se cambia logica de captura de datos

--- views/showSearchResults.php
+++ views/showSearchResults.php
@@ -1,14 +1,15 @@
 <?php
     //var_dump($argv);
-    parse_str($argv[1], $site);
-    parse_str($argv[2], $date);
-    var_dump($site);
-    var_dump($date);
     define('ROOT_DIR', dirname(__FILE__,2));
     require_once ROOT_DIR.'/controllers/getDataArc.php';
     if(isset($_GET["start"]) && isset($_GET["site"])){
         autoBucle($_GET["site"],$_GET["start"]);
-    }elseif(array_key_exists('start',$site) && array_key_exists('site',$date)){
+    }
+    if(count($argv)>1){
+        parse_str($argv[1], $site);
+        parse_str($argv[2], $date);
+        var_dump($site);
+        var_dump($date);
         autoBucle($site["site"],$date["start"]);
     }
 ?>
