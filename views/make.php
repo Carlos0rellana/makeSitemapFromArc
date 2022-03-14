f4b06cb0cf12d29a74772af6d3aa4bd69a2d30b9
Carlos Orellana <carlos.orellana@publimetro.cl>
2022-03-14 19:20:20 -0300

    se deja dominio en https

--- views/make.php
+++ views/make.php
@@ -3,7 +3,8 @@
     define('ROOT_DIR', dirname(__FILE__,2));
     require_once ROOT_DIR.'/controllers/getDataArc.php';
     if(isset($_GET["make"]) && isset($_GET["site"])){
-        if($_GET["make"]==='sitemapIndex'){
+        echo 'se tienen los parametros de creacion de sitemap index.';
+        if($_GET["make"]=='sitemapIndex'){
             $site = $_GET["site"];
             $fileUrl = ROOT_DIR.'/sitemaps/'.$site.'/';
             print_r(makeASitemapIndex($fileUrl,$site));
